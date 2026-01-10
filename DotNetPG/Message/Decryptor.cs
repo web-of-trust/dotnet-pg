4f2e7c53fa7ab1a8a550aee7e284386dc1fa3df3
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2026-01-10 15:44:22 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- DotNetPG/Message/Decryptor.cs
+++ DotNetPG/Message/Decryptor.cs
@@ -1,10 +1,9 @@
 // Copyright (c) Dot Net Privacy Guard Project. All rights reserved.
 // Licensed under the BSD 3-Clause License. See LICENSE in the project root for license information.
 
-using DotNetPG.Packet;
-
 namespace DotNetPG.Message;
 
+using Packet;
 using Type;
 using Org.BouncyCastle.Utilities;
 
